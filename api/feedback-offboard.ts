e7415a08f1021e2e215f60800a602084a6e24afb
Vivien F <vivien@netc.fr>
2025-12-20 18:37:23 +0100

    changed server for vercel api route

---
+++ api/feedback-offboard.ts
@@ -0,0 +1,42 @@
+import nodemailer from 'nodemailer';
+
+console.log("Starting server...")
+console.log(process.env.MAIL_HOST)
+let transporter = nodemailer.createTransport({
+    host: process.env.MAIL_HOST as string,
+    port: Number(process.env.MAIL_PORT),
+    secure: true,
+    auth: {
+        user: process.env.MAIL_USER as string,
+        pass: process.env.MAIL_PASSWORD as string
+    }
+});
+
+try {
+    transporter.verify().then(() => {
+        console.log("Server is ready to take our messages")
+    }).catch(err => {
+        console.log("Failed to connect to the mail server", err)
+    })
+} catch (e) {
+    console.log("Failed to connect to the mail server", e)
+}
+
+export default {
+    fetch(req: Request) {
+        if(req.method !== "POST") return new Response("Method not allowed", {status: 405})
+        req.json().then((json: any) => {
+            console.log(json)
+            const mailContent = json.mailContent
+            transporter.sendMail({
+                from: '"Emojeezer Uninstallation" <>',
+                to: 'vivien@netc.fr',
+                subject: 'Feedback on Emojeezer Uninstallation',
+                text: mailContent,
+            }).catch(e => {
+                console.error("Failed to send the mail", e)
+                return new Response("Failed to send the mail", {status: 500})
+            }).then(() => new Response("Sent!"))
+        });
+    }
+}

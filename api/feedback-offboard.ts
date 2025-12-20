2715446f455081c946fa11fbc97b26f4ea19e35f
Vivien F <vivien@netc.fr>
2025-12-20 18:59:11 +0100

    changed server to async function

--- api/feedback-offboard.ts
+++ api/feedback-offboard.ts
@@ -23,23 +23,26 @@ try {
 }
 
 export default {
-    fetch(req: Request) {
+    async fetch(req: Request) {
         if(req.method !== "POST") return new Response("Method not allowed", {status: 405})
-        req.json().then((json: any) => {
-            const mailContent = json.mailContent
+        const json = await req.json() as any
+        if(!json.mailContent) return new Response("Missing mailContent", {status: 400})
+        console.log("Received mail")
+        const mailContent = json.mailContent
+        try {
             console.log("Sending mail:")
             transporter.sendMail({
                 from: '"Emojeezer Uninstallation" <emojeezer@choucroute_melba.github.io>',
                 to: 'vivien@netc.fr, pnom5939@gmail.com',
                 subject: 'Feedback on Emojeezer Uninstallation',
                 text: mailContent,
-            }).catch(e => {
-                console.error("Failed to send the mail", e)
-                return new Response("Failed to send the mail", {status: 500})
-            }).then(() => {
-                console.log("Sent!")
-                return new Response("Sent!")
-            })
-        });
+            });
+        } catch (e) {
+            console.error("Failed to send the mail", e)
+            return new Response("Failed to send the mail", {status: 500})
+        }
+
+        console.log("Sent!")
+        return new Response("Sent!")
     }
 }
